659ceb1c7dc78cad6c9fc1ddb18395c46fea8d35
Agus9217 <justinibarrola92@gmail.com>
2023-12-11 23:28:52 -0300

    haciendo testing

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,10 +1,14 @@
-import { Server } from '../src/presentation/server'
+import { envs } from '../src/config/envs';
+import { Server } from '../src/presentation/server';
 
-jest.mock('../src/presentation/server')
+jest.mock('../src/presentation/server');
 
-
-describe('testing App.ts', () => {
+describe('Should call server with argumetns and start', () => {
   test('should work', async () => {
-    await import ('../src/app')
+    await import('../src/app');
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenLastCalledWith({
+      port: envs.PORT
+    });
   });
 });
